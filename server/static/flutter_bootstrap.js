21da3d127fb82702a5ceaf2031a71f967a9634dc
dmpotter1361 <dmpotter1361@gmail.com>
2026-06-30 20:22:38 -0400

    v0.8.0: block editor + calendar + witchy moon favicon
    - SVG crescent moon favicon (purple/gold palette, star dots)
    - pubspec version bump to 0.8.0+8

--- server/static/flutter_bootstrap.js
+++ server/static/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a10d8ac38de835021c8d2f920dbf50a920ccc0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1234116730" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1845640933" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
